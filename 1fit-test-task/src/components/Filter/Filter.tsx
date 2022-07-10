c8bd59a14d719867f2bfe93a849afd89e92da7bb
Bonapart <mibuchi773@mail.ru>
2022-07-11 02:06:41 +0600

    feat: add filter component

--- 1fit-test-task/src/components/Filter/Filter.tsx
+++ 1fit-test-task/src/components/Filter/Filter.tsx
@@ -1,3 +1,25 @@
-export default function Filter() {
-  return <div></div>
+import { useContext } from "react";
+import { SkillContext } from "../../context/context";
+import { Skill } from "../../interfaces/Skill";
+import removeIcon from "../../assets/images/ui/remove.svg";
+
+type Props = {
+  skill: Skill;
+};
+
+export default function Filter({ skill }: Props) {
+  const filterState = useContext(SkillContext);
+  const removeFilter = () => {
+    skill.isActive = false;
+    filterState.updateSkills(skill);
+  }
+
+  return (
+    <li className="filter__item">
+      <h4 className="filter__item-name">{skill.name}</h4>
+      <button className="filter__item-remove" onClick={removeFilter}>
+        <img src={removeIcon} alt="Кнопка удаления фильтра" />
+      </button>
+    </li>
+  );
 }
--- 1fit-test-task/src/components/Filter/FilterList.tsx
+++ 1fit-test-task/src/components/Filter/FilterList.tsx
@@ -1,9 +1,24 @@
 import { useContext } from "react";
+import Filter from "./Filter";
 import { SkillContext } from "../../context/context";
 import { Skill } from "../../interfaces/Skill";
 
 export default function FilterList() {
-  const state = useContext(SkillContext);
+  const filterState = useContext(SkillContext);
+  const clearFilters = () => filterState.clearSkills();
 
-  return (<div>{state.skills?.map((skill: Skill) => <div>{skill.name}</div>)}</div>)
+  return (
+    <>
+      {filterState.skills?.length !== 0 && (
+        <div className="filters">
+          <ul className="filters__list">
+            {filterState.skills?.map((skill: Skill) => (
+              <Filter key={skill.name} skill={skill} />
+            ))}
+          </ul>
+          <button className="filters__clear" onClick={clearFilters}>Clear</button>
+        </div>
+      )}
+    </>
+  );
 }
